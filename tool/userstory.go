239a81bfd3270569d27ac8e8f9379f323326cca6
siongui <siongui@gmail.com>
2021-01-17 04:30:13 +0800

    type change

--- tool/userstory.go
+++ tool/userstory.go
@@ -14,9 +14,9 @@ func main() {
 		return
 	}
 
-	id := flag.Int64("id", 25025320, "id of instagram user")
+	id := flag.String("id", "25025320", "id of instagram user")
 	flag.Parse()
 
 	fmt.Println("Download unexpired stories (last 24 hours) of the user (no postlive)")
-	mgr.DownloadUserStoryPostlive(*id)
+	mgr.DownloadUserStory(*id)
 }
