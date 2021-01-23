54ef817905d8bfa92d36e413b0582df116abfaa6
siongui <siongui@gmail.com>
2021-01-24 03:48:27 +0800

    move code to siongui/instago

--- tool/dltxt.go
+++ tool/dltxt.go
@@ -3,35 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strconv"
 
 	"github.com/siongui/instago/download"
 )
-
-func DownloadStoryFromUserIdFile(useridfile string, interval int64, verbose bool, mgr *igdl.IGDownloadManager) (err error) {
-	idstrs, err := igdl.ReadNonCommentLines(useridfile)
-	if err != nil {
-		return
-	}
-
-	// usually there are at most 150 trays in reels_tray.
-	// double the buffer to 300. 160 or 200 may be ok as well.
-	c := make(chan igdl.TrayInfo, 300)
-
-	for _, idstr := range idstrs {
-		id, err := strconv.ParseInt(idstr, 10, 64)
-		if err != nil {
-			return err
-		}
-
-		// layer = 2: also download reel mentions in story item
-		c <- igdl.SetupTrayInfo(id, "", 2, false)
-	}
-
-	mgr.TrayDownloaderViaStoryAPI(c, igdl.NewTimeLimiter(interval), true, true, verbose)
-
-	return
-}
 
 func main() {
 	mgr, err := igdl.NewInstagramDownloadManager("auth-clean.json")
@@ -43,7 +17,7 @@ func main() {
 	f := flag.String("f", "users.txt", "file containing user id")
 	flag.Parse()
 
-	err = DownloadStoryFromUserIdFile(*f, 12, true, mgr)
+	err = mgr.DownloadStoryFromUserIdFile(*f, 12, true)
 	if err != nil {
 		fmt.Println(err)
 		return
